0aa102baec41e93574c3bcaf527667882b1bb57b
nachoofg <plstsuchi@gmail.com>
2025-09-10 23:51:51 -0300

    fix: add id to link with css

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function Footer() {
   return (
-    <section className="flex items-center justify-center w-full p-4">
+    <section id="footer" className="flex items-center justify-center w-full p-4">
       <h2>@nnachonesa ~ 16.02.2023</h2>
     </section>
   );
