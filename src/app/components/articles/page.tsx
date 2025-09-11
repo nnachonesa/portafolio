0ebc9e9abf1aed1e802eba94c07b12e7c60c9457
nachoofg <plstsuchi@gmail.com>
2025-09-11 01:25:21 -0300

    feat: add article automatization

--- src/app/components/articles/page.tsx
+++ src/app/components/articles/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { articles } from "@/data/articles.json";
+
 export default function Articles() {
   return (
     <section
@@ -10,66 +12,22 @@ export default function Articles() {
       </h2>
 
       <div className="max-w-3xl mx-auto space-y-4">
-        <a
-          href="articles/quicksort.html"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-amber-600 flex-shrink-0"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M10 4H2v16h20V6H12l-2-2z" />
-          </svg>
-          <div>
-            <h3 className="font-semibold">Quicksort</h3>
-            <p className="text-sm">
-              Quicksort es un algoritmo de ordenamiento por comparación
-              recursivo que divide una lista de entrada en dos sublistas según
-              un pivote seleccionado
-            </p>
-          </div>
-        </a>
-        <a
-          href="articles/fibonacci.html"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-amber-600 flex-shrink-0"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M10 4H2v16h20V6H12l-2-2z" />
-          </svg>
-          <div>
-            <h3 className="font-semibold">Fibonacci</h3>
-            <p className="text-sm">
-              La Serie Fibonacci se define como una secuencia de números en la
-              que cada número después de los dos primeros es la suma de los dos
-              números anteriores.
-            </p>
-          </div>
-        </a>
-        <a
-          href="articles/luhn.html"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-amber-600 flex-shrink-0"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M10 4H2v16h20V6H12l-2-2z" />
-          </svg>
-          <div>
-            <h3 className="font-semibold">Algoritmo de Luhn</h3>
-            <p className="text-sm">
-              El algoritmo de Luhn, también conocido como la fórmula de Luhn, es
-              un método de suma de verificación utilizado ampliamente para
-              validar una variedad de números de identificación.
-            </p>
-          </div>
-        </a>
+        {articles.map((article) => (
+          <a href={article.src} key={article.title}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-amber-600 flex-shrink-0"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10 4H2v16h20V6H12l-2-2z" />
+            </svg>
+            <div>
+              <h3 className="font-semibold">{article.title}</h3>
+              <p className="text-sm">{article.desc}</p>
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
