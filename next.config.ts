a59b90e732556440d6642e4d646c4b2dcfa2a752
nnachonesa <imfigliuolo@gmail.com>
2026-08-10 00:39:46 -0300

    refactor: edit settings

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
-import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-     webpack(config) {
-        config.resolve.fallback = {
-            fs: false,
-            path: false,
-            os: false,
-            ...config.resolve.fallback,
-        };
-        return config;
-    }, 
-    devIndicators: false,
-    images: {
-        remotePatterns: [
-            new URL("https://avatars.githubusercontent.com/u/76927919?v=4"),
-            {
-                protocol:'https',
-                hostname:'https://i.pinimg.com'
-            },
-            {
-                protocol:'https',
-                hostname:'i.imgur.com'
-            }
-        ],
-    },
-};
+const withNextIntl = createNextIntlPlugin();
 
-export default nextConfig;
+const nextConfig = {};
+
+export default withNextIntl(nextConfig);
